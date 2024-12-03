11141e5165c39183ad225799ee50744cc4c74af7
nathsara <saranath2006@gmail.com>
2024-12-03 16:09:36 -0500

    Update simulation.py
    changed disk to flat cylinder

--- simulation.py
+++ simulation.py
@@ -1,8 +1,8 @@
 from vpython import *
 
 scene = canvas(width=400, height=400, background=vec(1, 1, 1))
-earth = ellipsoid(pos=vec(0, 0, 0), length=100, height=10, width=100, color=color.blue)
-earth.rotate(axis=vec(0, 1, 0), angle=pi / 3, origin=vec(0, 0, 0))
+earth = cylinder(pos=vec(0, 0, 0), axis=vec(100, 0, 0), color=color.yellow)
+earth.rotate(angle=-pi / 2, origin=vec(0, 0, 0))
 
 while True:
     pass
