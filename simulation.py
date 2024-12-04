e7cb44df98374d6178af6036869af7bf8a46d221
nathsara <saranath2006@gmail.com>
2024-12-04 17:46:43 +0000

    added linear motion of ball

--- simulation.py
+++ simulation.py
@@ -1,8 +1,17 @@
 from vpython import *
 
 scene = canvas(width=400, height=400, background=vec(1, 1, 1))
-earth = cylinder(pos=vec(0, 0, 0), axis=vec(100, 0, 0), color=color.yellow)
+earth = cylinder(pos=vec(0, 0, 0), axis=vec(0, 0, 300), color=color.yellow)
 earth.rotate(angle=-pi / 2, origin=vec(0, 0, 0))
+
+particle = sphere(pos=vec(-200, 0, 0), radius=10, color=color.yellow, make_trail=True)
+velocity = vec(0.1, 0, 0)
+dt = 0.1
+
+while particle.pos.x < 400:
+    rate(30)
+    particle.pos += particle.pos + velocity*dt
+
 
 while True:
     pass
