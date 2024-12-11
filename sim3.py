66326b291e148326a151d1fc35f2c738df7447cf
An Qi Ke <kea@bxscience.edu>
2024-12-11 04:04:31 -0500

    added print positions every sec back in & played around with params in sim3

--- sim3.py
+++ sim3.py
@@ -1,6 +1,6 @@
 from vpython import *
 
-def multi_particles_on_disk(g, rotation_speed, ball_mass, num_particles_per_layer, num_layers):
+def multi_particles_on_rotating_disk(g, rotation_speed, ball_mass, num_particles_per_layer, num_layers):
     # Set up the scene
     scene = canvas()
     scene.caption = (
@@ -9,13 +9,13 @@ def multi_particles_on_disk(g, rotation_speed, ball_mass, num_particles_per_laye
         "Touch screen: pinch/extend to zoom, swipe or two-finger rotate."
     )
 
-    # Create the disk (representing the ground or reference plane)
-    floor = cylinder(pos=vector(0, -8, 0), axis=vector(0, 0.01, 0), radius=100, color=color.blue)
+    # Create the rotating disk (representing the ground or reference plane)
+    floor = cylinder(pos=vector(0, 0, 0), axis=vector(0, 0.01, 0), radius=200, color=color.blue) # TODO revert y for pos vector to be -8
 
     # Time parameters
     time = 0
     dt = 0.001
-    t_f = 10  # simulate for 10 seconds
+    t_f = 20  # simulate for 10 seconds
     frequency = 1/dt
 
     # Angular velocity vector of the tornado (about the y-axis)
@@ -24,13 +24,17 @@ def multi_particles_on_disk(g, rotation_speed, ball_mass, num_particles_per_laye
     # Gravity vector
     g_vec = vector(0, -g, 0)
 
-    # Parameters for layering
-    # Assume top is smaller radius and each layer moves upward
+    ### Parameters for layering
+    # constraints
+    min_height = 0
+    min_radius = 1
+
+    # Assume top is bigger radius and each layer moves upward
     top_radius = 20
     bottom_radius = floor.radius
     height_per_layer = 5  # vertical spacing between layers
     radius_step = (bottom_radius - top_radius) / (num_layers - 1)
-
+    
     particles = []
     for layer in range(num_layers):
         # Compute the radius for this layer
@@ -44,14 +48,14 @@ def multi_particles_on_disk(g, rotation_speed, ball_mass, num_particles_per_laye
             z_pos = current_radius * sin(angle)
 
             # Create the particle
-            particle = sphere(pos=vector(x_pos, y_pos, z_pos), radius=1, color=color.red)
+            particle = sphere(pos=vector(x_pos, y_pos, z_pos), radius=1, color=color.red, make_trail=True)
 
             # Initial tangential velocity: v = ω × r
             r_vector = particle.pos - floor.pos
             tan_vel = cross(rotation_speed_vec, r_vector)
 
             # Add a vertical upward velocity component to get them started
-            tan_vel += vector(0,10,0)
+            # tan_vel += vector(0,10,0)
 
             particle.velocity = tan_vel
 
@@ -116,7 +120,7 @@ def print_stats(accel, vel, v_mag, pos):
 
 # Example call with parameters
 # multi_particles_on_rotating_disk(g=9.8, rotation_speed=0.5, ball_mass=0.001, num_particles_per_layer=10, num_layers=5)
-multi_particles_on_disk(g=9.8, rotation_speed=2, ball_mass=0.0001, num_particles_per_layer=10, num_layers=20)
+multi_particles_on_rotating_disk(g=9.8, rotation_speed=1, ball_mass=0.0001, num_particles_per_layer=10, num_layers=3)
 
 # Call with user_inputs
 # user_input()
